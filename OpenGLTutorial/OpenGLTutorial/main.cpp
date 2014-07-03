2466ebe32c47a93f4fc46d5b17b35990b71fbdd1
Thiago <thiagobernardes10@gmail.com>
2014-07-03 19:34:32 -0300

    Project Created

---
+++ OpenGLTutorial/OpenGLTutorial/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenGLTutorial
+//
+//  Created by Thiago Bernardes on 7/3/14.
+//  Copyright (c) 2014 TMB. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
